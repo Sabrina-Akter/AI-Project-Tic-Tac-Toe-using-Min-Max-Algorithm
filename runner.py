1663761c26d11307d36a2e36259628d9908fa556
Sabrina-Akter <sabrinahossain202@gmail.com>
2022-04-03 22:02:17 +0600

    commit 2

--- runner.py
+++ runner.py
@@ -1,16 +1,32 @@
-from operator import truediv
 import pygame
- 
+import sys
+import time
+
+import tictactoe as ttt
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 pygame.init()
- 
-display = pygame.display.set_mode((800,600))
-pygame.display.update()
- 
-open = True
-while open:
+size = width, height = 600, 400
+
+# Colors
+black = (0, 0, 0)
+white = (246, 239, 119)
+
+screen = pygame.display.set_mode(size)
+
+mediumFont = pygame.font.SysFont("OpenSans-Regular.ttf", 28)
+largeFont = pygame.font.SysFont("OpenSans-Regular.ttf", 40)
+moveFont = pygame.font.SysFont("OpenSans-Regular.ttf", 60)
+
+user = None
+board = ttt.initial_state()
+ai_turn = False
+
+while True:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            open = False
- 
-pygame.quit()
-quit()
+            sys.exit()
+
+    screen.fill(black)
